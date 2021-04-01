c42ab95ba6385cc0afdece395bcca729a0ebdf63
dAFI <dafinadavid@gmail.com>
2021-04-01 08:59:25 +0300

    swaped the for with find method, extracted the handleCheckbox logic in a separate function, added event prssing Enter key, rearanged the code

--- main.js
+++ main.js
@@ -1,80 +1,81 @@
-(function () {
-  // Select DOM Elements:
-  const pantryInput = document.querySelector(".pantry-input");
-  const pantryAddBtn = document.querySelector(".pantry-add-btn");
-  const pantryList = document.querySelector(".list");
+// Select DOM Elements:
+const pantryInput = document.querySelector(".pantry-input");
+const pantryAddBtn = document.querySelector(".pantry-add-btn");
+const pantryList = document.querySelector(".pantry-list");
 
-  // Implement a dynamic value for the id of each new item and save all added items in local storage
-  let localPantry = [];
+// Save pantry list in local storage:
+let localPantry = [];
+const savedPantry = localStorage.getItem("localPantry");
+if (savedPantry) {
+  localPantry = JSON.parse(savedPantry);
+}
 
-  const savedPantry = localStorage.getItem("localPantry");
-  if (savedPantry) {
-    localPantry = JSON.parse(savedPantry);
+// Add item in pantryList, update localPantry and render the pantry list:
+function addToPantry() {
+  if (pantryInput.value !== "") {
+    localPantry.push({ itemName: pantryInput.value, isChecked: false });
+
+    updateLocalPantry();
+    renderPantryList();
   }
 
-  /* Add a new item on the screen in pantryList, push it in the array used to save item in the local storage
-     and update the local storage with the renew array: */
-  function addToPantry() {
-    if (pantryInput.value !== "") {
-      localPantry.push({ itemName: pantryInput.value, isChecked: false });
+  pantryInput.value = "";
+}
 
-      updateLocalPantry();
-      renderPantryList();
-    }
+// Create and configure pantry item:
+function renderItem(itemVal, isChecked, index) {
+  let inputId = `pantry-item-${index + 1}`;
 
-    pantryInput.value = "";
+  const pantryItem = document.createElement("div");
+  pantryItem.classList.add("pantry-item");
+
+  const input = document.createElement("input");
+  input.type = "checkbox";
+  input.id = inputId;
+  input.checked = isChecked;
+  input.value = itemVal;
+  input.addEventListener("change", handleCheckbox);
+
+  const label = document.createElement("label");
+  label.textContent = itemVal;
+  label.setAttribute("for", inputId);
+
+  pantryItem.appendChild(input);
+  pantryItem.appendChild(label);
+
+  return pantryItem;
+}
+
+// Empty pantryList & Populate pantryList with all items from local storage:
+function renderPantryList() {
+  pantryList.innerHTML = "";
+  localPantry.forEach(function (itemVal, index) {
+    const item = renderItem(itemVal.itemName, itemVal.isChecked, index);
+    pantryList.appendChild(item);
+  });
+}
+
+// Update localStorage:
+function updateLocalPantry() {
+  localStorage.setItem("localPantry", JSON.stringify(localPantry));
+}
+
+function handleCheckbox(e) {
+  const found = localPantry.find(function (i) {
+    return i.itemName === e.target.value;
+  });
+  found.isChecked = e.target.checked;
+
+  updateLocalPantry();
+}
+
+// Add event handlers:
+document.addEventListener("DOMContentLoaded", renderPantryList);
+pantryAddBtn.addEventListener("click", addToPantry);
+document.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    addToPantry();
   }
-
-  // Populate pantryList with all items from local storage:
-  function renderPantryList() {
-    pantryList.innerHTML = "";
-    localPantry.forEach(function (itemVal, index) {
-      const item = renderItem(itemVal.itemName, itemVal.isChecked, index);
-
-      pantryList.appendChild(item);
-    });
-  }
-
-  // Update localStorage:
-  function updateLocalPantry() {
-    localStorage.setItem("localPantry", JSON.stringify(localPantry));
-  }
-
-  // Create and configure pantry item:
-  function renderItem(itemVal, isChecked, index) {
-    let inputId = `pantry-item-${index + 1}`;
-
-    const pantryItem = document.createElement("div");
-    pantryItem.classList.add("pantry-item");
-
-    const input = document.createElement("input");
-    input.type = "checkbox";
-    input.id = inputId;
-    input.checked = isChecked;
-    input.value = itemVal;
-    input.addEventListener("change", function (e) {
-      for (const itemVal of localPantry) {
-        if (itemVal.itemName === e.target.value) {
-          itemVal.isChecked = e.target.checked;
-          updateLocalPantry();
-          break;
-        }
-      }
-    });
-
-    const label = document.createElement("label");
-    label.textContent = itemVal;
-    label.setAttribute("for", inputId);
-
-    pantryItem.appendChild(input);
-    pantryItem.appendChild(label);
-
-    return pantryItem;
-  }
-
-  // Add event handlers:
-  document.addEventListener("DOMContentLoaded", renderPantryList);
-  pantryAddBtn.addEventListener("click", addToPantry);
-})();
+});
 
 // localStorage.clear();
